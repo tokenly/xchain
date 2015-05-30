16427b1cd6262f7b5b6dafb3a75ee702ee0b3628
Devon Weller <dweller@devonweller.com>
2015-05-30 08:20:07 -0500

    clear compiled classes before updating

--- .deploy.sh
+++ .deploy.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+# clear the compiled classes
+if [ -f storage/framework/compiled.php] ; then
+    rm storage/framework/compiled.php
+fi
+
+
 /usr/local/bin/composer.phar install --prefer-dist --no-progress
 # ./artisan migrate
