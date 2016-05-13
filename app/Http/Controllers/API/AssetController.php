93b7c92744f3b778c53caba17ecec9d762ecdf2e
Devon Weller <devon@tokenly.co>
2016-05-12 21:05:35 -0500

    Fixed error handling in asset info controller

--- app/Http/Controllers/API/AssetController.php
+++ app/Http/Controllers/API/AssetController.php
@@ -7,6 +7,7 @@ use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 use Tokenly\CounterpartyAssetInfoCache\Cache;
+use Tokenly\LaravelEventLog\Facade\EventLog;
 
 class AssetController extends APIController {
 	
@@ -17,12 +18,12 @@ class AssetController extends APIController {
 	 * */
 	public function get(Cache $asset_info_cache, $asset)
 	{
-		$asset = $asset_info_cache->get(strtoupper($asset));
-		if(!$asset){
-            $message = "The asset $asset could not be found";
+		$asset_info = $asset_info_cache->get(strtoupper($asset));
+		if (!$asset_info) {
+            $message = "The asset $asset was not found";
             EventLog::logError('error.getAsset', ['asset' => $asset, 'message' => $message]);
-            return new JsonResponse(['message' => $message], 500); 
+            return new JsonResponse(['message' => $message], 404); 
 		}
-		return json_encode($asset);
+		return json_encode($asset_info);
 	}
 }
