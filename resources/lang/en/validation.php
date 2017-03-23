ad03ad1d64a7ceb943e1737add186da7e04a1d0c
Devon Weller <devon@tokenly.co>
2017-03-22 19:05:15 -0500

    add asset validation error text

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -4,6 +4,7 @@ return [
 
     // custom
     'fee_priority' => 'The :attribute must be a valid fee priority like low, medium, high or a number between 1 and 1000.',
+    'asset'        => 'The :attribute must be a valid token name.',
 
 	/*
 	|--------------------------------------------------------------------------
