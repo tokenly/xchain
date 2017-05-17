87887e7e1c145c961fcccfac1c3310b7b4939d80
Devon Weller <devon@tokenly.co>
2017-05-17 06:41:03 -0500

    Add XCHAIN_BACKFILL_MAX setting

--- config/xchain.php
+++ config/xchain.php
@@ -3,7 +3,7 @@
 return [
 
 	// number of blocks max to backfill in the event of an outage
-	'backfill_max' => 450,
+	'backfill_max' => env('XCHAIN_BACKFILL_MAX', 450),
 
     // should log debug timing
     'debugLogTxTiming' => !!env('DEBUG_LOG_TX_TIMING', false),
