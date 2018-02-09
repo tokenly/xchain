3536510c6544077cf1341b1fea071ce4d2c3502a
Devon Weller <devon@tokenly.com>
2018-02-09 10:44:24 -0600

    update find address command

--- app/Console/Commands/Address/FindAddressCommand.php
+++ app/Console/Commands/Address/FindAddressCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands\Address;
 
+use App\Models\PaymentAddressArchive;
 use Exception;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -50,7 +51,9 @@ class FindAddressCommand extends Command {
     protected function getOptions()
     {
         return [
-            // ['inactive', 'i', InputOption::VALUE_NONE, 'Include inactive accounts'],
+            ['uuid', 'u', InputOption::VALUE_NONE, 'Show UUID only'],
+            ['archived', null, InputOption::VALUE_NONE, 'Show archived only'],
+            ['live', 'l', InputOption::VALUE_NONE, 'Show live only'],
         ];
     }
 
@@ -62,21 +65,73 @@ class FindAddressCommand extends Command {
      */
     public function fire()
     {
+        $uuid_only = $this->input->getOption('uuid');
         DB::transaction(function() {
 
             $payment_address_repo = app('App\Repositories\PaymentAddressRepository');
 
             $address = $this->input->getArgument('address');
-            $payment_addresses = $payment_address_repo->findByAddress($address)->get();
-            if (!$payment_addresses->count()) { throw new Exception("Payment address not found for address $address", 1); }
-            $this->info("found {$payment_addresses->count()} address".($payment_addresses->count() > 1 ? "es" : ""));
+            $uuid_only = $this->input->getOption('uuid');
+            $archived_only = $this->input->getOption('archived');
+            $live_only = $this->input->getOption('live');
 
-            $payment_addresses->each(function($payment_address) {
-                $this->comment("Address {$payment_address['address']} (UUID: {$payment_address['uuid']} ID: {$payment_address['id']})");
-            });
+            $found = false;
+
+            if (!$archived_only) {
+                $payment_addresses = $payment_address_repo->findByAddress($address)->get();
+                if ($payment_addresses->count()) {
+                    if ($uuid_only AND $payment_addresses->count() > 1) {
+                        throw new Exception("Found multiple addresses", 1);
+                    }
+                    $found = true;
+        
+                    if (!$uuid_only) {
+                        $this->info("found {$payment_addresses->count()} address".($payment_addresses->count() > 1 ? "es" : ""));
+                    }
+
+                    $payment_addresses->each(function($payment_address) use ($uuid_only) {
+                        if ($uuid_only) {
+                            $this->line($payment_address['uuid']);
+                        } else {
+                            $this->comment("Address {$payment_address['address']} (UUID: {$payment_address['uuid']} ID: {$payment_address['id']})");
+                        }
+                    });
+                }
+
+            }
+
+            // try archived if not found
+            if (!$found AND !$live_only) {
+                $payment_addresses = PaymentAddressArchive::where('address', $address)->get();
+
+                if ($payment_addresses->count()) {
+                    if ($uuid_only AND $payment_addresses->count() > 1) {
+                        throw new Exception("Found multiple ARCHIVED addresses", 1);
+                    }
+                    $found = true;
+                
+                    if (!$uuid_only) {
+                        $this->info("found {$payment_addresses->count()} ARCHIVED address".($payment_addresses->count() > 1 ? "es" : ""));
+                    }
+
+                    $payment_addresses->each(function($payment_address) use ($uuid_only) {
+                        if ($uuid_only) {
+                            $this->line($payment_address['uuid']);
+                        } else {
+                            $this->comment("ARCHIVED Address {$payment_address['address']} (UUID: {$payment_address['uuid']} ID: {$payment_address['id']})");
+                        }
+                    });
+                }
+            }
+
+            if (!$found) {
+                throw new Exception("Payment address not found for address $address", 1);
+            }
         });
 
-        $this->info('done');
+        if (!$uuid_only) {
+            $this->info('done');
+        }
     }
 
 
